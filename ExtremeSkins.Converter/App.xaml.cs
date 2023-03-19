4ea9be63a15573fe0e7c80c73fda795323286481
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-19 19:54:59 +0900

    プロジェクト ファイルを追加します。

---
+++ ExtremeSkins.Converter/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ExtremeSkins.Converter.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace ExtremeSkins.Converter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
