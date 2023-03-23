e190a417772bbf0781b77ee4fb3b80e926c0cbd9
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-23 21:28:35 +0900

    refactor

--- ExtremeSkins.Converter.Service/OpenExplorerService.cs
+++ ExtremeSkins.Converter.Service/OpenExplorerService.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using ExtremeSkins.Converter.Service.Interface;
-
-namespace ExtremeSkins.Converter.Service;
+﻿namespace ExtremeSkins.Converter.Service;
 
 public sealed class OpenExplorerService
 {
