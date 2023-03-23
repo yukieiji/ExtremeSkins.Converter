463e7bc661d0082623b825a5449b48ef9f77fc89
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-03-23 09:13:41 +0900

    feat:ToHAnalyzer

--- ExtremeSkins.Converter.Core/Analyzer/TheOtherRoles/TheOtherHatsAnalyzer.cs
+++ ExtremeSkins.Converter.Core/Analyzer/TheOtherRoles/TheOtherHatsAnalyzer.cs
@@ -1,5 +1,11 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using ExtremeSkins.Converter.Core.Interface;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using ExtremeSkins.Converter.Core.Extension;
 
 using static ExtremeSkins.Converter.Core.Analyzer.TheOtherRoles.Define;
 
@@ -16,12 +22,52 @@ public sealed class TheOtherHatsAnalyzer : IRepositoryAnalyzer
 
     public AnalyzeResult Analyze()
     {
-        throw new System.NotImplementedException();
+        using StreamReader hatJsonReader = File.OpenText(Path.Combine(TargetPath, HatDataJson));
+        JObject hatJson = JObject.Load(new JsonTextReader(hatJsonReader));
+
+        List<ExtremeHatConverter> hatConverter = new List<ExtremeHatConverter>();
+        if (hatJson.TryGetValue(HatDataBodyKey, out JToken hatToken) &&
+            hatToken is JArray hatDataArray)
+        {
+            foreach (JToken hat in hatDataArray)
+            {
+                hatConverter.Add(
+                    new ExtremeHatConverter()
+                    {
+                        Author = hat.Value<string>(HatAuthorKey),
+                        Name = hat.Value<string>(HatNameKey),
+                        FrontImagePath = GetImagePath(hat, HatFrontImgKey, HatDataFolder),
+                        FrontFlipImagePath = GetImagePath(hat, HatFrontFlipImgKey, HatDataFolder),
+                        BackImagePath = GetImagePath(hat, HatBackImgKey, HatDataFolder),
+                        BackFlipImagePath = GetImagePath(hat, HatBackFlipImgKey, HatDataFolder),
+                        ClimbImagePath = GetImagePath(hat, HatClimbImgKey, HatDataFolder),
+                        IsBound = hat.TryGetValue(HatBounceKey, out bool isHatBounce) && isHatBounce,
+                        IsShader = hat.TryGetValue(HatAdaptiveKey, out bool isHatShader) && isHatShader,
+                    }
+                );
+            }
+        }
+        return new AnalyzeResult()
+        {
+            Name = "TheOtherHat",
+            Hat = hatConverter,
+            NamePlate = new(),
+            Visor = new(),
+        };
     }
 
     public bool IsValid()
     {
         string hatJsonPath = Path.Combine(TargetPath, HatDataJson);
         return File.Exists(hatJsonPath);
+    }
+
+    private string GetImagePath(JToken token, string key, string folder)
+    {
+        string value = token.GetStringValue(key);
+
+        if (string.IsNullOrEmpty(value)) { return string.Empty; }
+
+        return Path.Combine(this.TargetPath, folder, value);
     }
 }
